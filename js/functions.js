78fd26de77320d6e8276bf1dc14370b8fb95171c
Jaimy Nouris <jaimynouris@gmail.com>
2017-07-09 06:08:17 +0200

    Added ability to drag Cards inside the grid and order them

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,35 @@
 $(document).ready(function() {
-    	$("header .settings, .card.global-settings .dismiss").click(function() {
-        		$(".card.global-settings").toggle();
-    	});
-    	$("header .tools .search, .card.search .dismiss").click(function() {
-        		$(".card.search").toggle();
-    	});
-    	$("header .tools .help, .card.help .dismiss").click(function() {
-        		$(".card.help").toggle();
-    	});
+	$("header .settings, .card.global-settings .dismiss").click(function() {
+		$(".card.global-settings").toggle();
+	});
+	$("header .tools .search, .card.search .dismiss").click(function() {
+		$(".card.search").toggle();
+	});
+	$("header .tools .help, .card.help .dismiss").click(function() {
+		$(".card.help").toggle();
+	});
 
-    	$(".dismissable .dismiss").click(function() {
-        		$(this).parents(".dismissable").remove();
-    	});
+	$(".dismissable .dismiss").click(function() {
+		$(this).parents(".dismissable").remove();
+	});
 
-    	$(".raids .toolbar .clear").click(function() {
-    		$(".raids .collection-table-items .collection-table-item").remove();
-    	});
+	$(".raids .toolbar .clear").click(function() {
+		$(".raids .collection-table-items .collection-table-item").remove();
+	});
 
-    	$(".settings-toggle").click(function() {
-    		$(this).parents(".card").children(".raid-settings").toggle();
-    	});
+	$(".settings-toggle").click(function() {
+		$(this).parents(".card").children(".raid-settings").toggle();
+	});
 
+	var pckry = new Packery('.grid', {
+		itemSelector: '.grid-item',
+		percentPosition: true,
+		columnWidth: 25,
+		rowHeight: 25
+	});
+
+	pckry.getItemElements().forEach(function(itemElem) {
+		var draggie = new Draggabilly(itemElem);
+		pckry.bindDraggabillyEvents(draggie);
+	});
 });

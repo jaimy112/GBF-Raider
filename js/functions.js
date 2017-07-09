f6c909a334beaeacc40a9578e32406ae05842dd2
Jaimy Nouris <jaimynouris@gmail.com>
2017-07-09 02:29:10 +0200

    Added vertical layout Cards. Slight styling tweaks.

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    	$("header .settings, .card.settings .dismiss").click(function() {
-        		$(".card.settings").toggle();
+    	$("header .settings, .card.global-settings .dismiss").click(function() {
+        		$(".card.global-settings").toggle();
     	});
     	$("header .tools .search, .card.search .dismiss").click(function() {
         		$(".card.search").toggle();
@@ -15,5 +15,10 @@ $(document).ready(function() {
 
     	$(".raids .toolbar .clear").click(function() {
     		$(".raids .collection-table-items .collection-table-item").remove();
-    	})
+    	});
+
+    	$(".settings-toggle").click(function() {
+    		$(this).parents(".card").children(".raid-settings").toggle();
+    	});
+
 });
